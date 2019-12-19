6ac31be926ba65d5075de92db3878c6e392a193e
Nakusha <runnway@naver.com>
2019-12-19 16:06:38 +0900

    finsh Slider 3/3

--- components/MovieSlide.js
+++ components/MovieSlide.js
@@ -1,13 +1,38 @@
 import React from "react";
 import PropTypes from "prop-types";
 import styled from "styled-components";
+import makePhotoUrl from "../Utils/makePhotoUrl";
+import Layout from "../constants/Layout";
+import MoviePoster from "./MoviePoster";
 
 const Container = styled.View`
+    flex: 1;
+    position: relative;
 `;
 
-const BgImage = styled.Image``;
+const BgImage = styled.Image`
+    width:${Layout.width};
+    height:${Layout.height / 3};
+    opacity: 0.5;
+    position: absolute;
+`;
 
-const MovieSlide = ({id, posterPhoto, backgroundPhoto, title ,voteAvrg, overView}) => <Container><BgImage /></Container>;
+const Content = styled.View`
+    flex: 1;
+    flex-direction: row;
+    align-items: center;
+    padding-horizontal: 30px;
+    justify-content: space-between;
+`;
+
+const MovieSlide = ({id, posterPhoto, backgroundPhoto, title ,voteAvrg, overView}) => (
+    <Container>
+        <BgImage source={{uri: makePhotoUrl(backgroundPhoto)}}/>
+        <Content>
+            <MoviePoster path={posterPhoto}/>
+        </Content>
+    </Container>
+);
 
 MovieSlide.propTypes = {
     id: PropTypes.number.isRequired,
